fb39637a3a8d6f86f05212452f019606f3bfe27e
sysani <russo.anitaa@gmail.com>
2020-06-29 12:04:22 -0400

    adding minor css and app changes

--- app.py
+++ app.py
@@ -12,18 +12,20 @@ def index():
 def success_table():
     if request.method=="POST":
         file=request.files['file']
-        try:
-            df=pandas.read_csv(file)
-            gc=Nominatim(user_agent="app.py")
-            df['coordinates']=df['Address'].apply(gc.geocode)
-            df['Latitude']=df['coordinates'].apply(lambda x: x.latitude if x != None else None)
-            df['Longitude']=df['coordinates'].apply(lambda x: x.longitude if x != None else None)
-            df=df.drop('coordinates',1)
-            df.to_csv("uploads/geocoded.csv", index=None)
-            return render_template("index.html", text=df.to_html(), btn="download.html")
-        except:
-            return render_template("index.html", text="Please enter a file with an address column")
+        df=pandas.read_csv(file)
+        return render_table(df)
 
+def render_table(df):
+    try:
+        gc=Nominatim(user_agent="app.py")
+        df['coordinates']=df['Address'].apply(gc.geocode)
+        df['Latitude']=df['coordinates'].apply(lambda x: x.latitude if x != None else None)
+        df['Longitude']=df['coordinates'].apply(lambda x: x.longitude if x != None else None)
+        df=df.drop('coordinates',1)
+        df.to_csv("uploads/geocoded.csv", index=None)
+        return render_template("index.html", text=df.to_html(), btn="download.html")
+    except:
+        return render_template("index.html", text="Please enter a file with an address column")
 
 @app.route('/download-file/')
 def download():
